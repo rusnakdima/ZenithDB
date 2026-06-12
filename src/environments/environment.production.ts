7497fae22db2df101e886f54f261147c3d863b6f
Dmitriy303 <rusnakdima03@gmail.com>
2026-06-13 02:04:52 +0500

    chore: update config files and environment

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  logger: {
+    enabled: true,
+    debug: false,
+    info: false,
+    warn: true,
+    error: true,
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  logger: {
+    enabled: true,
+    debug: true,
+    info: true,
+    warn: true,
+    error: true,
+  },
+};
